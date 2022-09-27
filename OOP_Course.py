d3362a13d62479a2e320d23aeb406675a634d121
joao <joao-morrudo@hotmail.com.br>
2022-09-26 21:00:30 -0300

    Update

--- OOP_Course.py
+++ OOP_Course.py
@@ -1,4 +1,9 @@
-from .item import Item
-from .phone import Phone
+from item import Item
+from phone import Phone
 
 
+item1 = Item("MyItem", 750)
+
+item1.name = "OtherName"
+
+print(item1.name)
--- item.py
+++ item.py
@@ -11,12 +11,23 @@ class Item:
         assert quantity >= 0, f"Quantity {quantity} não é maior ou igual a zero!"
 
         # definindo o objeto
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         # Ações
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception("The name is too long!")
+        else:
+            self.__name = value
 
     def calculate_total_price(self):
         return self.price * self.quantity
